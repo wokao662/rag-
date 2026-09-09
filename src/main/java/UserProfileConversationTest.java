1083d6f483e6e5186f6a0aa40a9a967fdc3a62e0
wokao662 <3336212991@qq.com>
2026-09-09 18:16:32 +0800

    Update UserProfileConversationTest.java

--- src/main/java/UserProfileConversationTest.java
+++ src/main/java/UserProfileConversationTest.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.example.rag.AppConfig;
 import com.example.rag.profile.ConversationRepository;
 import com.example.rag.profile.ConversationalProfileAgent;
 import com.example.rag.profile.MessageRepository;
